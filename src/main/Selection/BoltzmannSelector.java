9e0159e4857cc09364e2498ba80c45132ef2c0c4
Ezequiel Keimel <ekeimel@hotmail.com>
2020-04-27 00:48:41 -0300

    -.-

---
+++ src/main/Selection/BoltzmannSelector.java
@@ -0,0 +1,54 @@
+package main.Selection;
+
+import main.GeneticSubject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class BoltzmannSelector implements Selector {
+    private double T0 = 10.0;
+    private double Tc = 2.0;
+    private int t = 0;
+
+    @Override
+    public List<GeneticSubject> select(List<GeneticSubject> characters, int K) {
+        List<GeneticSubject> l = new ArrayList<>();
+        if(K == 0)
+            return l;
+
+        List<GeneticSubject> orderedCharacters = new ArrayList<>(characters);
+        Collections.sort(orderedCharacters);
+
+        double r[] = new double[K];
+        for(int i = 0; i < K; i++)
+            r[i] = GeneticSubject.random.nextFloat();
+        Arrays.sort(r);
+
+        double f[] = new double[orderedCharacters.size()];
+        double sum = 0;
+        double T = getT();
+        for(int i = 0; i < orderedCharacters.size(); i++) {
+            f[i] = Math.exp(orderedCharacters.get(i).getFitness() / T);
+            sum += f[i];
+        }
+        for(int i = 0; i < orderedCharacters.size(); i++)
+            f[i] = f[i] / sum;
+
+        double currQ = 0;
+        for(int i = 0; l.size() < K && i < characters.size(); i++) {
+            currQ += f[i];
+            while(l.size() < K && currQ > r[l.size()])
+                l.add(characters.get(i));
+        }
+
+        return l;
+    }
+
+    private double getT() {
+        double T = Tc + (T0 - Tc) * Math.exp(-0.1 * t);
+        t++;
+        return T;
+    }
+}
--- src/main/Selection/RankingSelector.java
+++ src/main/Selection/RankingSelector.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-//TODO Roulette y Universal promedian fitness 24, este promedia fitness 17???
 public class RankingSelector implements Selector {
     @Override
     public List<GeneticSubject> select(List<GeneticSubject> characters, int K) {
@@ -19,8 +18,13 @@ public class RankingSelector implements Selector {
         Collections.sort(orderedCharacters);
 
         double f[] = new double[orderedCharacters.size()];
-        for(int i = 0; i < orderedCharacters.size(); i++)
+        double sum = 0;
+        for(int i = 0; i < orderedCharacters.size(); i++) {
             f[i] = (double)i / orderedCharacters.size();
+            sum += f[i];
+        }
+        for(int i = 0; i < orderedCharacters.size(); i++)
+            f[i] = f[i] / sum;
 
         double r[] = new double[K];
         for(int i = 0; i < K; i++)
@@ -28,12 +32,10 @@ public class RankingSelector implements Selector {
         Arrays.sort(r);
 
         double currQ = 0;
-        double lastQ = 0;
         for(int i = 0; l.size() < K && i < characters.size(); i++) {
-            currQ = f[i] + lastQ;
+            currQ += f[i];
             while(l.size() < K && currQ > r[l.size()])
                 l.add(characters.get(i));
-            lastQ = currQ;
         }
 
         return l;
